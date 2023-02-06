85399d242f8360bbde84d909f8469d0468d3b829
Anna Vesslén <anna_ananaz@hotmail.com>
2023-02-06 09:56:05 +0100

    Added GameRepository + interface

---
+++ Lms.Data/Repositories/GameRepository.cs
@@ -0,0 +1,78 @@
+﻿using Lms.Core.Entities;
+using Lms.Core.Repositories;
+using Lms.Data.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Data.Repositories
+{
+    public class GameRepository : IGameRepository
+    {
+       
+            private LmsApiContext db;
+
+            public GameRepository(LmsApiContext db)
+            {
+                this.db = db;
+            }
+
+
+            public async Task<IEnumerable<Game>> GetAllAsync()
+            {
+                return await db.Game.ToListAsync();
+            }
+
+
+            public async Task<Game> GetAsync(int id)
+            {
+                ArgumentNullException.ThrowIfNull(id, nameof(id));
+                return await db.Game.FirstOrDefaultAsync(g => g.Id == id);
+            }
+
+
+            public async Task<bool> AnyAsync(int id)
+            {
+                return await (db.Game.AnyAsync(g => g.Id == id));
+            }
+
+            public void Add(Game game)
+            {
+                if (game is null)
+                {
+                    throw new ArgumentNullException(nameof(game));
+                }
+
+                db.AddAsync(game);
+            }
+
+            public void Update(Game game)
+            {
+                if (game is null)
+                {
+                    throw new ArgumentNullException(nameof(game));
+                }
+
+                db.SaveChangesAsync();
+            }
+
+            public void Remove(Game game)
+            {
+                if (game is null)
+                {
+                    throw new ArgumentNullException(nameof(game));
+                }
+
+                db.Game.Remove(game);
+            }
+        }
+
+
+
+
+
+    }
+
